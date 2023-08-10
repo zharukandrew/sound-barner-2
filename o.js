342062be6a82a55ba0b1c44cb46c10998c31b7f6
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-08-10 13:02:48 +0400

    добавил инфу

--- o.js
+++ o.js
@@ -1,13 +1,21 @@
-class Human {
-   constructor(firstName, lastName) {
-       this.firstName = firstName
-       this.lastName = lastName
+function Person(first,last) {
+    return {
+        first,
+        last
+    }
    }
+   // Что указывается далее: A, Б или В?
+   const friend1 = new Person("Laurence", "Svekis");
+   console.log( `привет`,friend1.first);
+   console.log(friend1.last);
+  /* function Human(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
 }
-const chris = new Human('Chris', 'Coyier')
+const chris = Human('Chris', 'Coyier')
 
 console.log(chris.firstName) // Chris
 console.log(chris.lastName) // Coyier
-const zell = new Human('Zell', 'Liew')
-console.log(zell.firstName) // Zell
-console.log(zell.lastName) // Liew
+*/
--- z.js
+++ z.js
@@ -1,36 +1,25 @@
-/*let a=prompt('введите число');
-let b=prompt('введите число');
-let c= Number(a)+Number(b);
-if(c==19){
-    alert('Верно')
-}else{
-    alert('Неверно')
-}*/
+
 let a = document.querySelector(".input");
 let b = document.querySelector(".inputs");
-let d = document.querySelector('.button');
-d.addEventListener("click", () => {
-  let c = Number(a.value) + Number(b.value);
-  alert(c)
-});
-let one = document.querySelector(".one");
-let two = document.querySelector(".two");
-let minus = document.querySelector('.minus');
-minus.addEventListener("click", () => {
-  let p = Number(one.value) - Number(two.value);
-  alert(p)
-});
-let arr = document.querySelector(".arr");
-let brr = document.querySelector(".brr");
-let drr = document.querySelector('.drr');
-drr.addEventListener("click", () => {
-  let crr = Number(arr.value) * Number(brr.value);
-  alert(crr);
-});
-let mar = document.querySelector(".mar");
-let mbr = document.querySelector(".mbr");
-let mdr = document.querySelector('.mdr');
-mdr.addEventListener("click", () => {
-  let mcr = Number(mar.value) / Number(mbr.value);
-  alert(mcr);
-});
+let d = document.querySelectorAll('.button');
+
+
+  
+    
+function multiply () {
+  alert(Number(a.value) * Number(b.value))
+}
+function fold () {
+  alert(Number(a.value) + Number(b.value))
+}
+function subtract () {
+  alert(Number(a.value) - Number(b.value))
+}
+function divide () {
+  alert(Number(a.value) / Number(b.value))
+}
+
+    
+  
+
+
