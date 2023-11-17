b8bd181c13afd9c17accb5350c81d449e9856898
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-11-17 13:57:09 +0400

    hhhh

--- o.js
+++ o.js
@@ -1,20 +1,3 @@
 // JSON.stringify({name: 'a', age: 29}) Делает строку из объекта
 // JSON.parse() Делает объект из строки
 
-fetch('https://jsonplaceholder.typicode.com/photos?_start=0&_limit=5')
-     .then((res) => res.json())
-     .then((date) => viePhoto(date))
-
-function viePhoto (arr) {
-    console.log(arr)
-    arr.forEach(element => {
-        let img = document.createElement('img')
-            img.src = element.url
-            img.classList.add('img')
-        let p = document.createElement('p')
-            p.textContent = element.title
-        document.body.append(img)
-        document.body.append(p)
-    });
-}
-
---
+++ z.js
@@ -0,0 +1,17 @@
+
+
+/*async function N(){
+    try {
+        let res = await fetch('https://jsonplaceholder.typicode.com/photos')
+        let data  = await res.json()
+        console.log(data)
+    }
+   catch(err){
+    console.log('j')
+   }
+   finally{
+    console.log(u)
+   }
+    }
+    N()*/
+   
