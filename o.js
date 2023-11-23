eb138f781c8a34fc410cc1a8e96cad584dc6213f
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-11-23 11:07:12 +0400

    jjj

--- o.js
+++ o.js
@@ -1,3 +1,40 @@
 // JSON.stringify({name: 'a', age: 29}) Делает строку из объекта
 // JSON.parse() Делает объект из строки
 
+// Реализовать часы на странице 
+// По клику получить актуальную дату 
+// Пеменять текуший цвет дива (выбрать из существующих) input type='color' 
+
+    /*let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+   
+      if (hours < 10) hours = "0" + hours;
+      if (minutes < 10) minutes = "0" + minutes;
+      if (seconds < 10) seconds = "0" + seconds;
+       let a = document.querySelector(".clock")
+       setTimeout(() => { console.log(a.innerHTML) }, 1000)
+       a.innerHTML = hours + ":" + minutes + ":" + seconds;*/
+       let date = new Date();
+       let hours = date.getHours();
+       let minutes = date.getMinutes();
+       let seconds = date.getSeconds();
+       //* добавление ведущих нулей */
+       if (hours < 10) hours = "0" + hours;
+       if (minutes < 10) minutes = "0" + minutes;
+       if (seconds < 10) seconds = "0" + seconds;
+       let a = document.querySelector(".clock");
+       a.innerHTML = hours + ":" + minutes + ":" + seconds;
+       
+       setInterval(() => {
+         date = new Date();
+         hours = date.getHours();
+         minutes = date.getMinutes();
+         seconds = date.getSeconds();
+         //* добавление ведущих нулей */
+         if (hours < 10) hours = "0" + hours;
+         if (minutes < 10) minutes = "0" + minutes;
+         if (seconds < 10) seconds = "0" + seconds;
+         a.innerHTML = hours + ":" + minutes + ":" + seconds;
+       }, 1000);
