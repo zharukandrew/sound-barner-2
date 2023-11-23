0631e6ca7840109b5a58106ab23887d35e50a529
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-11-23 13:19:30 +0400

    hhhh

--- o.js
+++ o.js
@@ -16,29 +16,41 @@
        let a = document.querySelector(".clock")
        setTimeout(() => { console.log(a.innerHTML) }, 1000)
        a.innerHTML = hours + ":" + minutes + ":" + seconds;*/
-       let b = document.querySelector(".buttons")
-       b.addEventListener("click",()=>{
-        let date = new Date();
-       let hours = date.getHours();
-       let minutes = date.getMinutes();
-       let seconds = date.getSeconds();
-       
-       if (hours < 10) hours = "0" + hours;
-       if (minutes < 10) minutes = "0" + minutes;
-       if (seconds < 10) seconds = "0" + seconds;
-       let a = document.querySelector(".clock");
-       a.innerHTML = hours + ":" + minutes + ":" + seconds;
-       
-       setInterval(() => {
-         date = new Date();
-         hours = date.getHours();
-         minutes = date.getMinutes();
-         seconds = date.getSeconds();
-        
+       let b = document.querySelector(".buttons");
+       b.addEventListener("click", () => {
+         let date = new Date();
+         let hours = date.getHours();
+         let minutes = date.getMinutes();
+         let seconds = date.getSeconds();
          if (hours < 10) hours = "0" + hours;
          if (minutes < 10) minutes = "0" + minutes;
          if (seconds < 10) seconds = "0" + seconds;
+         let a = document.querySelector(".clock");
          a.innerHTML = hours + ":" + minutes + ":" + seconds;
-       }, 1000);
-       })
+         setInterval(() => {
+           date = new Date();
+           hours = date.getHours();
+           minutes = date.getMinutes();
+           seconds = date.getSeconds();
+           if (hours < 10) hours = "0" + hours;
+           if (minutes < 10) minutes = "0" + minutes;
+           if (seconds < 10) seconds = "0" + seconds;
+           a.innerHTML = hours + ":" + minutes + ":" + seconds;
+         }, 1000);
+       });
        
+       let red = document.querySelector(".red");
+       let blue = document.querySelector(".blue");
+       let green = document.querySelector(".green");
+       red.addEventListener("click", () => {
+         let a = document.querySelector(".clock");
+         a.classList.toggle("red");
+       });
+       blue.addEventListener("click", () => {
+        let a = document.querySelector(".clock");
+        a.classList.toggle("blue");
+      });
+      green.addEventListener("click", () => {
+        let a = document.querySelector(".clock");
+        a.classList.toggle("green");
+      });
