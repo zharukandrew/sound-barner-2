3e2f2bbac2089c5458a25d9482abbad4fcb31792
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-11-23 13:19:53 +0400

    jjjj

--- o.js
+++ o.js
@@ -1,21 +1,4 @@
-// JSON.stringify({name: 'a', age: 29}) Делает строку из объекта
-// JSON.parse() Делает объект из строки
 
-// Реализовать часы на странице 
-// По клику получить актуальную дату 
-// Пеменять текуший цвет дива (выбрать из существующих) input type='color' 
-
-    /*let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-   
-      if (hours < 10) hours = "0" + hours;
-      if (minutes < 10) minutes = "0" + minutes;
-      if (seconds < 10) seconds = "0" + seconds;
-       let a = document.querySelector(".clock")
-       setTimeout(() => { console.log(a.innerHTML) }, 1000)
-       a.innerHTML = hours + ":" + minutes + ":" + seconds;*/
        let b = document.querySelector(".buttons");
        b.addEventListener("click", () => {
          let date = new Date();
