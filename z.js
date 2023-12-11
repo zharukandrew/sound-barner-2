13a4c353521c39a1ca97c943e141aff270968a7a
andrew.zharuck@gmail.com <andrew.zharuck@gmail.com>
2023-12-11 12:39:17 +0400

    добавил код

--- z.js
+++ z.js
@@ -1,37 +1,106 @@
+//1
+// const arr = ["Камень", "Ножницы", "Бумага" ];
+// const startButton = document.querySelector('.game3-button1');
+// const computerAnswer = document.querySelector('.game3-page');
+// const buttons = document.querySelectorAll('.button');
+// let resultText = document.querySelector('.page');
+
+// let mapEventov = {
+//   'Ножницы': {
+//     'Бумага': 0,
+//     'Камень': 1,
+//     'Ножницы': 2
+//   },
+//   'Бумага': {
+//     'Бумага': 2,
+//     'Камень': 1,
+//     'ножницы': 0
+//   },
+//   'Камень': {
+//     'Бумага': 0,
+//     'Камень': 2,
+//     'Ножницы': 1
+//   },
+// }
+
+// startButton.addEventListener('click', () => {
+//   computerAnswer.innerHTML = "Компьютер готов к игре!";
+// });
+
+// buttons.forEach((btn) => {
+//    btn.addEventListener('click', (e) => {
+//     const userChoice = e.target.innerHTML
+//     const computerChoice = arr[Math.floor(Math.random() * arr.length)];
+//     resultText.innerHTML = `Вы выбрали: ${userChoice} <br>
+//                 Компьютер выбрал: ${computerChoice} <br>`;
+
+//     if (mapEventov[userChoice][computerChoice] === 1) {
+//       resultText.innerHTML += 'Вы выиграли'
+//     } else if (mapEventov[userChoice][computerChoice] === 2) {
+//       resultText.innerHTML += 'Ничья'
+//     } else if (mapEventov[userChoice][computerChoice] === 0) {
+//       resultText.innerHTML += 'Вы проиграли'
+//     } else {
+//       resultText.innerHTML += 'Что то пошло не так'
+//     }
+//   });
+// })
+//1
+
+
 const arr = ["Камень", "Ножницы", "Бумага"];
-const playButton = document.querySelector('.game3-button');
 const startButton = document.querySelector('.game3-button1');
 const computerAnswer = document.querySelector('.game3-page');
 const buttons = document.querySelectorAll('.button');
+let resultText = document.querySelector('.page');
+
+class Games {
+  constructor(mapEventov) {
+    this.mapEventov = mapEventov;
+  }
+  play() {
+    console.log(this.mapEventov);
+  }
+}
+
+let games = new Games({
+  'Ножницы': {
+    'Бумага': 0,
+    'Камень': 1,
+    'Ножницы': 2
+  },
+  'Бумага': {
+    'Бумага': 2,
+    'Камень': 1,
+    'Ножницы': 0
+  },
+  'Камень': {
+    'Бумага': 0,
+    'Камень': 2,
+    'Ножницы': 1
+  }
+});
+
+games.play();
+
 startButton.addEventListener('click', () => {
   computerAnswer.innerHTML = "Компьютер готов к игре!";
 });
 
-playButton.addEventListener('click', () => {
-  const userChoice = document.querySelector('.button.active').innerHTML;
-  const computerChoice = arr[Math.floor(Math.random() * arr.length)];
-  console.log(`Вы выбрали: ${userChoice}`);
-  console.log(`Компьютер выбрал: ${computerChoice}`);
-
-  if (userChoice === computerChoice) {
-    computerAnswer.innerHTML = "Ничья!";
-  } else if (
-    (userChoice === "Камень" && computerChoice === "Ножницы") ||
-    (userChoice === "Ножницы" && computerChoice === "Бумага") ||
-    (userChoice === "Бумага" && computerChoice === "Камень")
-  ) {
-    computerAnswer.innerHTML = "Вы выиграли!";
-  } else {
-    computerAnswer.innerHTML = "Вы проиграли!";
-  }
-});
-
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    buttons.forEach(btn => btn.classList.remove('active'));
-    button.classList.add('active');
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const userChoice = e.target.innerHTML;
+    const computerChoice = arr[Math.floor(Math.random() * arr.length)];
+    resultText.innerHTML = `Вы выбрали: ${userChoice} <br>
+                Компьютер выбрал: ${computerChoice} <br>`
+    if (games.mapEventov[userChoice][computerChoice] === 1) {
+      resultText.innerHTML += 'Вы выиграли!';
+    } else if (games.mapEventov[userChoice][computerChoice] === 2) {
+      resultText.innerHTML += 'Ничья!';
+    } else if (games.mapEventov[userChoice][computerChoice] === 0) {
+      resultText.innerHTML += 'Вы проиграли!';
+    } else {
+      resultText.innerHTML += 'Что-то пошло не так!';
+    }
   });
 });
-
-      
